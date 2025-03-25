09df66fa4ed358ee644d5624d3c36f4adcd7b5e9
Peter Cashel <pacas00@gmail.com>
2025-03-26 06:43:37 +1000

    Cleanup for rewrite

--- src/main/java/net/petercashel/dingusprimeacm/kubejs/types/gameboy/IForceSaving.java
+++
@@ -1,7 +0,0 @@
-package net.petercashel.dingusprimeacm.kubejs.types.gameboy;
-
-import net.petercashel.dingusprimeacm.kubejs.types.gameboy.client.emulation.GameboyFileBattery;
-
-public interface IForceSaving {
-    void ForcedSave(GameboyFileBattery battery);
-}
