fec20652f3cd8c94e40b04531ccc2ad34fdb8b71
Peter Cashel <pacas00@gmail.com>
2025-03-29 21:47:50 +1000

    Fix UUID, Creative no longer breaks IDs

--- src/main/java/net/petercashel/dingusprimeacm/kubejs/types/gameboy/item/GameBoyCartItem.java
+++ src/main/java/net/petercashel/dingusprimeacm/kubejs/types/gameboy/item/GameBoyCartItem.java
@@ -1,11 +1,20 @@
 package net.petercashel.dingusprimeacm.kubejs.types.gameboy.item;
 
 import dev.latvian.mods.kubejs.item.ItemBuilder;
+import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.common.MutableDataComponentHolder;
 import net.petercashel.dingusprimeacm.DingusPrimeAdditionalContentMod;
 import net.petercashel.dingusprimeacm.DingusRegistries;
 import net.petercashel.dingusprimeacm.kubejs.types.gameboy.datacomponent.CartItemDataComponent;
@@ -18,11 +27,62 @@ public class GameBoyCartItem extends Item {
         super(properties);
     }
 
+    @Override
+    public DataComponentMap components() {
+        return super.components();
+    }
+
+    @Override
+    public int getDefaultMaxStackSize() {
+        return 1;
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+
+        var stack = context.getItemInHand();
+        if (stack != null && stack.isEmpty() == false && stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+            CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+
+
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+
+        if (player != null) {
+            var stack = player.getItemInHand(usedHand);
+            if (stack != null && stack.isEmpty() == false && stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+                CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+                stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+            }
+        }
+
+        return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+        super.inventoryTick(stack, level, entity, slotId, isSelected);
+    }
+
+    @Override
+    public void onCraftedPostProcess(ItemStack stack, Level level) {
+        super.onCraftedPostProcess(stack, level);
+        if (stack != null && stack.isEmpty() == false && stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+            CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+    }
+
     public String gameID = "defaultrom";
 
     public GameBoyCartItem(GBCartridgeBuilder builder) {
         super(builder.createItemProperties()
-            .component(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT, new CartItemDataComponent(builder.gameID, UUID.randomUUID().toString()))
+            //.component(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT, new CartItemDataComponent(builder.gameID, UUID.randomUUID().toString()))
         );
 
         gameID = builder.gameID;
@@ -35,8 +95,61 @@ public class GameBoyCartItem extends Item {
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
         super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
-        //Add tooltip here
+        //Ensure on hover that we have the component
+        if (stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get())) {
+            CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            if (!stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get())) {
+                stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+            }
 
+            if (tooltipFlag.isAdvanced()) {
+                tooltipComponents.add(Component.literal("Game: " + data.GameID()));
+                tooltipComponents.add(Component.literal("UUID: " + data.UniqueID()));
+            }
+        } else if (tooltipFlag.isCreative() == false){
+            CartItemDataComponent data = new CartItemDataComponent(gameID, UUID.randomUUID().toString());
+            stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+    }
+
+    @Override
+    public ItemStack getDefaultInstance() {
+        var stack = super.getDefaultInstance();
+        return stack;
+    }
+
+    @Override
+    public boolean onDroppedByPlayer(ItemStack item, Player player) {
+
+        if (item != null && item.isEmpty() == false && item.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+            CartItemDataComponent data = item.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            item.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+        return super.onDroppedByPlayer(item, player);
+    }
+
+    @Override
+    public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
+
+        if (stack != null && stack.isEmpty() == false && stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+            CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+        return super.onItemUseFirst(stack, context);
+    }
+
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+        if (stack != null && stack.isEmpty() == false && stack.getComponents().has(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get()) == false) {
+            CartItemDataComponent data = stack.getComponents().getOrDefault(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), new CartItemDataComponent(gameID, UUID.randomUUID().toString()));
+            stack.set(DingusPrimeAdditionalContentMod.CART_ITEM_DATA_COMPONENT.get(), data);
+        }
+        return super.onLeftClickEntity(stack, player, entity);
+    }
+
+    @Override
+    public int getMaxStackSize(ItemStack stack) {
+        return 1;
     }
 
 
